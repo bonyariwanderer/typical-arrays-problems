885331ac2ad11db3c1e32d930a409f84f6a01c98
Julia Bazaeva <truckfulofcandy@gmail.com>
2021-08-16 20:35:00 +0700

    feat: add functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,34 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(!array || !array.length) {
+    return 0;
+  } else {
+    return Math.min.apply(null, array);
+    }  
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(!array || !array.length) {
+    return 0;
+  } else {
+    return Math.max.apply(null, array);
+    }  
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(!array || !array.length) {
+    return 0;
+  } else {
+      let sum = 0;
+      for(let i = 0; i < array.length; i++) {
+        sum += parseInt(array[i], 10);  
+      }
+      let result = sum / array.length;
+      return result;
+    }
 }
+
+
+
+  
+ 
